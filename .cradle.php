c043d43d09694fd7d4af70fc73ad5a96b40c8b22
Christian Blanquera <cblanquera@openovate.com>
2019-01-15 11:44:38 +0800

    updating syntax to 2.2 standard

--- .cradle.php
+++ .cradle.php
@@ -1,44 +1,9 @@
 <?php //-->
-
-use Cradle\Http\Request;
-use Cradle\Http\Response;
-
 /**
- * Loads CSRF token in stage
+ * This file is part of a package designed for the CradlePHP Project.
  *
- * @param *Request  $request
- * @param *Response $response
+ * Copyright and license information can be found at LICENSE.txt
+ * distributed with this package.
  */
-$cradle->on('csrf-load', function (Request $request, Response $response) {
-    //render the key
-    $key = md5(uniqid());
-    if($request->hasSession('csrf')) {
-        $key = $request->getSession('csrf');
-    }
 
-    $request->setSession('csrf', $key);
-    $response->setResults('csrf', $key);
-});
-
-/**
- * Validates CSRF
- *
- * @param *Request  $request
- * @param *Response $response
- */
-$cradle->on('csrf-validate', function (Request $request, Response $response) {
-    $actual = $request->getStage('csrf');
-    $expected = $request->getSession('csrf');
-
-    //no longer needed
-    $request->removeSession('csrf');
-
-    if($actual !== $expected) {
-        //prepare to error
-        $message = 'We prevented a potential attack on our servers coming from the request you just sent us.';
-        $message = $this->package('global')->translate($message);
-        $response->setError(true, $message);
-    }
-
-    //it passed
-});
+require_once __DIR__ . '/src/events.php';
